66a0c5a3a000aab7bec3f0ec16c35824279d3af0
Alex Emrie <emrieaj@gmail.com>
2017-02-13 20:35:17 -0800

    Takes an optional second argument

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var sumReduce = function(array){
+var sumReduce = function(array, optInitialValue){
+
+    var initValue = typeof optInitialValue == 'number' ? optInitialValue : 0;
 
     var sum = array.reduce(function(prev, curr){
         return prev + curr;
-    });
+    }, initValue);
 
     return sum;
 };
--- test/reduce_sum_spec.js
+++ test/reduce_sum_spec.js
@@ -2,8 +2,25 @@ var assert = require('assert');
 var sumReduce = require('./../index');
 
 describe('reduce sum', function(){
-   it ('reduces the sum of all values in an array', function(){
-      var numberArray = [5,10];
-      assert.equal(sumReduce(numberArray), 15);
-   });
+    it('reduces the sum of all values in an array: one number', function(){
+        var numberArray = [5];
+        assert.equal(sumReduce(numberArray), 5);
+    });
+
+
+    it('reduces the sum of all values in an array: two numbers', function(){
+        var numberArray = [5,10];
+        assert.equal(sumReduce(numberArray), 15);
+    });
+
+    it('reduces the sum of all values in an array: three numbers', function(){
+        var numberArray = [10, 15, 20];
+        assert.equal(sumReduce(numberArray), 45);
+    });
+
+    it('takes an optional initial value as a second argument', function(){
+        var numberArray = [10, 20];
+        var initialValue = 7;
+        assert.equal(sumReduce(numberArray, initialValue), 37);
+    });
 });
